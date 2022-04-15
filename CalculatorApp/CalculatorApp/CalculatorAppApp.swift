10ece4d5fa94e0577cf957c696e14a55a40b8ad5
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2022-04-15 10:07:45 -0400

    added xcode project

---
+++ CalculatorApp/CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by LMCM on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
